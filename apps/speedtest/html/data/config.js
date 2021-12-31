bbdc8a01a12f8b59749c4deb45b606418a98da94
Marco Janker <marco.janker@gmail.com>
2021-12-31 16:01:50 +0100

    Modified

---
+++ apps/speedtest/html/data/config.js
@@ -0,0 +1,32 @@
+/**
+ * Configuration overrides. Specify any overrides you want here and they will be respected.
+ */
+/* eslint-disable no-undef */
+
+speedtestConfig.customTitle = "Speedtest Statistics",
+speedtestConfig.dateFormat = "YYYY-MM-DD";
+speedtestConfig.locale = "de";
+speedtestConfig.labels = {
+    download: "Hochladen",
+    ping: "Ping",
+    upload: "Upload"
+};
+// speedtestConfig.daterange = {
+//     timePicker: true,
+//     timePicker24Hour: true,
+//     startDate: moment().subtract(1, "month"),
+//     endDate: moment().endOf("day"),
+//     ranges: {
+//         Today: [moment().hours(0).minutes(0).seconds(0), moment().hours(23).minutes(59).seconds(59)],
+//         Yesterday: [
+//             moment().hours(0).minutes(0).seconds(0)
+//                 .subtract(1, "days"),
+//             moment().hours(23).minutes(59).seconds(59)
+//                 .subtract(1, "days")
+//         ],
+//         "Last 7 Days": [moment().subtract(6, "days"), moment()],
+//         "Last 30 Days": [moment().subtract(29, "days"), moment()],
+//         "This Month": [moment().startOf("month"), moment().endOf("month")],
+//         "Last Month": [moment().subtract(1, "month").startOf("month"), moment().subtract(1, "month").endOf("month")]
+//     }
+// };
